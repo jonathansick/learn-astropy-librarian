1e9558b023eb9cb6eea91a18b379d4958df1eee9
Jonathan Sick <jonathan@jsick.codes>
2021-05-13 11:13:26 -0400

    Add test files from ccd-guide
    This test dataset is from [A guide to CCD data reduction and stellar
    photometry using astropy and affiliated
    packages](https://github.com/astropy/ccd-reduction-and-photometry-guide)
    and is used to test ingesting a Jupyter Book-rendered guide.
    
    The index.html is used to identify the redirect page that Jupyter Book
    creates.
    
    The 00-00-Preface.html file is used to test parsing the homepage and the
    <nav> for a list of all content in the guide.
    
    The 01-05-Calibration-overview.html file is an example of a content page
    that has a mix of text, math, code, and images.

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,3 +44,59 @@ def coordinates_transform_tutorial() -> TestHtml:
         url="http://learn.astropy.org/rst-tutorials/"
         "Coordinates-Transform.html",
     )
+
+
+@pytest.fixture(scope="session")
+def ccd_guide_index() -> TestHtml:
+    """The ``ccd-guide/index.html`` page.
+
+    This page is the root file created by Jupyter Book, but which redirects
+    to the first content page (notebooks/00-00-Preface.html).
+    """
+    source_path = Path(__file__).parent / "data" / "ccd-guide" / "index.html"
+    return TestHtml(
+        html=source_path.read_text(),
+        url="http://www.astropy.org/ccd-reduction-and-photometry-guide/"
+        "index.html",
+    )
+
+
+@pytest.fixture(scope="session")
+def ccd_guide_00_00() -> TestHtml:
+    """The ``ccd-guide/notebooks/00-00-Preface.html`` page.
+
+    This is the CCD Guide homepage created by Jupyter Book.
+    """
+    source_path = (
+        Path(__file__).parent
+        / "data"
+        / "ccd-guide"
+        / "notebooks"
+        / "00-00-Preface.html"
+    )
+    return TestHtml(
+        html=source_path.read_text(),
+        url="http://www.astropy.org/ccd-reduction-and-photometry-guide/"
+        "notebooks/00-00-Preface.html",
+    )
+
+
+@pytest.fixture(scope="session")
+def ccd_guide_01_05() -> TestHtml:
+    """The ``ccd-guide/notebooks/01-05-Calibration-overview.html`` page.
+
+    This is a regular content page from the CCD Guide homepage created by
+    Jupyter Book.
+    """
+    source_path = (
+        Path(__file__).parent
+        / "data"
+        / "ccd-guide"
+        / "notebooks"
+        / "01-05-Calibration-overview.html"
+    )
+    return TestHtml(
+        html=source_path.read_text(),
+        url="http://www.astropy.org/ccd-reduction-and-photometry-guide/"
+        "notebooks/01-05-Calibration-overview.html",
+    )
